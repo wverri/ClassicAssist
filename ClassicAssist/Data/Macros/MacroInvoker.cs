de1edd1884ccdbb8c2066132773da1f79a840cab
Reetus <john.scott@outlook.com.au>
2019-11-24 20:04:01 +0800

    MacroInvoke Initial

---
+++ ClassicAssist/Data/Macros/MacroInvoker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using IronPython.Hosting;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+
+namespace ClassicAssist.Data.Macros
+{
+    public class MacroInvoker
+    {
+        private readonly MacroEntry _macro;
+        private readonly ScriptEngine _engine;
+        private ScriptScope _scope;
+
+        public delegate void dMacroStartStop();
+
+        public delegate void dMacroException( Exception e );
+
+        public event dMacroStartStop StartedEvent;
+        public event dMacroStartStop StoppedEvent;
+        public event dMacroException ExceptionEvent;
+
+        public MacroInvoker( MacroEntry macro )
+        {
+            _macro = macro;
+            _engine = Python.CreateEngine();
+            _scope = _engine.CreateScope();
+
+            ScriptRuntime runtime = _engine.Runtime;
+            runtime.LoadAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private string GetScriptingImports()
+        {
+            string prepend = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Namespace != null && t.IsPublic && t.IsClass && t.Namespace.EndsWith("Macros.Commands")).Aggregate(string.Empty, (current, t) => current + $"from {t.FullName} import * \n");
+
+            return prepend;
+        }
+
+        public bool IsRunning => Thread.IsAlive;
+        public bool IsFaulted { get; set; }
+        public Exception Exception { get; set; }
+
+        public void Execute()
+        {
+            string code = $"{GetScriptingImports()}\r\n{_macro.Macro}";
+
+            ScriptSource source = _engine.CreateScriptSourceFromString( code, SourceCodeKind.Statements );
+
+            Thread = new Thread( () =>
+            {
+                Thread = Thread.CurrentThread;
+
+                try
+                {
+                    StartedEvent?.Invoke();
+                    source.Execute();
+                }
+                catch ( ThreadAbortException )
+                {
+                }
+                catch ( Exception e )
+                {
+                    IsFaulted = true;
+                    Exception = e;
+
+                    ExceptionEvent?.Invoke( e );
+                }
+                finally
+                {
+                    StoppedEvent?.Invoke();
+                }
+            } ) { IsBackground = true };
+
+            Thread.Start();
+
+            while ( !Thread.IsAlive )
+            {
+                Thread.Sleep( 10 );
+            }
+        }
+
+        public void Stop()
+        {
+            Thread?.Abort();
+        }
+
+        public Thread Thread { get; set; }
+    }
+}
--- ClassicAssist/UI/ViewModels/MacrosTabViewModel.cs
+++ ClassicAssist/UI/ViewModels/MacrosTabViewModel.cs
@@ -1,6 +1,9 @@
-﻿using ClassicAssist.Data;
+﻿using System.Windows.Input;
+using ClassicAssist.Data;
 using ClassicAssist.Data.Hotkeys;
+using ClassicAssist.Data.Macros;
 using ClassicAssist.Misc;
+using ClassicAssist.UI.Misc;
 using Newtonsoft.Json.Linq;
 
 namespace ClassicAssist.UI.ViewModels
@@ -8,18 +11,63 @@ namespace ClassicAssist.UI.ViewModels
     public class MacrosTabViewModel : BaseViewModel, ISettingProvider
     {
         private HotkeyManager _hotkeys;
+        private bool _isPlaying;
+        private ObservableCollectionEx<MacroEntry> _items = new ObservableCollectionEx<MacroEntry>();
+        private ICommand _newMacroCommand;
+        private RelayCommand _removeMacroCommand;
+        private MacroEntry _selectedItem;
 
         public MacrosTabViewModel()
         {
             _hotkeys = HotkeyManager.GetInstance();
-
         }
+
+        public bool IsPlaying
+        {
+            get => _isPlaying;
+            set => SetProperty( ref _isPlaying, value );
+        }
+
+        public ObservableCollectionEx<MacroEntry> Items
+        {
+            get => _items;
+            set => SetProperty( ref _items, value );
+        }
+
+        public ICommand NewMacroCommand =>
+            _newMacroCommand ?? ( _newMacroCommand = new RelayCommand( NewMacro, o => !IsPlaying ) );
+
+        public ICommand RemoveMacroCommand =>
+            _removeMacroCommand ?? ( _removeMacroCommand =
+                new RelayCommand( RemoveMacro, o => !IsPlaying && SelectedItem != null ) );
+
+        public MacroEntry SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty( ref _selectedItem, value );
+        }
+
         public void Serialize( JObject json )
         {
         }
 
         public void Deserialize( JObject json, Options options )
         {
+        }
+
+        private void NewMacro( object obj )
+        {
+            int count = Items.Count;
+
+            Items.Add( new MacroEntry { Name = $"Macro-{count + 1}", Macro = string.Empty } );
+        }
+
+        private void RemoveMacro( object obj )
+        {
+            if ( obj is MacroEntry entry )
+            {
+                Items.Remove( entry );
+            }
         }
     }
 }
