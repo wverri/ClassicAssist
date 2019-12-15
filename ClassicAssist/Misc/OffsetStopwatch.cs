42b3486e555786e7a8bd2531fa2aa9bcbc3160b9
Reetus <reeeetus@gmail.com>
2019-12-15 12:58:33 +0800

    OffsetStopwatch

---
+++ ClassicAssist/Misc/OffsetStopwatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ClassicAssist.Misc
+{
+    public class OffsetStopwatch : Stopwatch
+    {
+        private TimeSpan _offset;
+
+        public OffsetStopwatch( TimeSpan offset )
+        {
+            _offset = offset;
+        }
+
+        public void Reset( TimeSpan offset )
+        {
+            _offset = offset;
+            Reset();
+        }
+
+        public new long ElapsedMilliseconds => base.ElapsedMilliseconds + (long) _offset.TotalMilliseconds;
+        public new long ElapsedTicks => base.ElapsedTicks + _offset.Ticks;
+    }
+}
