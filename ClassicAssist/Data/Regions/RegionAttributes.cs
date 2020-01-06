f68246a68df6312e7be46a62811ea40f2d62f66d
Reetus <reeeetus@gmail.com>
2020-01-07 05:59:42 +0800

    .

---
+++ ClassicAssist/Data/Regions/RegionAttributes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassicAssist.Data.Regions
+{
+    [Flags]
+    public enum RegionAttributes
+    {
+        None = 0,
+        Guarded = 1 << 1,
+        Jail = 1 << 2,
+        Wilderness = 1 << 3,
+        Dungeon = 1 << 4,
+        Special = 1 << 5,
+        Default = 1 << 6
+    }
+}
