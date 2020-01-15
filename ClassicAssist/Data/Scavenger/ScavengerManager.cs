eca068c68fcc85878a880e422439a7309b8e1228
Reetus <reeeetus@gmail.com>
2020-01-16 06:25:21 +0800

    .

---
+++ ClassicAssist/Data/Scavenger/ScavengerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using ClassicAssist.Data.Dress;
+
+namespace ClassicAssist.Data.Scavenger
+{
+    public class ScavengerManager
+    {
+        private static ScavengerManager _instance;
+        private static readonly object _instanceLock = new object();
+
+        public ObservableCollection<ScavengerEntry> Items { get; set; }
+
+        private ScavengerManager()
+        {
+            
+        }
+
+        public static ScavengerManager GetInstance()
+        {
+            // ReSharper disable once InvertIf
+            if (_instance == null)
+            {
+                lock (_instanceLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ScavengerManager();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+    }
+}
---
+++ ClassicAssist/UI/ViewModels/Agents/ScavengerTabViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Assistant;
+using ClassicAssist.Data;
+using ClassicAssist.Data.Scavenger;
+using ClassicAssist.Misc;
+using ClassicAssist.Resources;
+using ClassicAssist.UO.Data;
+using ClassicAssist.UO.Objects;
+using Newtonsoft.Json.Linq;
+using UOC = ClassicAssist.UO.Commands;
+
+namespace ClassicAssist.UI.ViewModels.Agents
+{
+    public class ScavengerTabViewModel : BaseViewModel, ISettingProvider
+    {
+        private readonly ScavengerManager _manager;
+        private ICommand _clearAllCommand;
+        private int _containerSerial;
+        private bool _enabled;
+        private ICommand _insertCommand;
+        private ObservableCollection<ScavengerEntry> _items = new ObservableCollection<ScavengerEntry>();
+        private ICommand _removeCommand;
+        private ScavengerEntry _selectedItem;
+        private ICommand _setContainerCommand;
+
+        public ScavengerTabViewModel()
+        {
+            _manager = ScavengerManager.GetInstance();
+            _manager.Items = Items;
+        }
+
+        public ICommand ClearAllCommand =>
+            _clearAllCommand ?? ( _clearAllCommand = new RelayCommandAsync( ClearAll, o => Items.Count > 0 ) );
+
+        public int ContainerSerial
+        {
+            get => _containerSerial;
+            set => SetProperty( ref _containerSerial, value );
+        }
+
+        public bool Enabled
+        {
+            get => _enabled;
+            set => SetProperty( ref _enabled, value );
+        }
+
+        public ICommand InsertCommand =>
+            _insertCommand ?? ( _insertCommand = new RelayCommandAsync( Insert, o => true ) );
+
+        public ObservableCollection<ScavengerEntry> Items
+        {
+            get => _items;
+            set => SetProperty( ref _items, value );
+        }
+
+        public ICommand RemoveCommand =>
+            _removeCommand ?? ( _removeCommand = new RelayCommandAsync( Remove, o => SelectedItem != null ) );
+
+        public ScavengerEntry SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty( ref _selectedItem, value );
+        }
+
+        public void Serialize( JObject json )
+        {
+        }
+
+        public void Deserialize( JObject json, Options options )
+        {
+        }
+
+        private async Task Insert( object arg )
+        {
+            int serial = await UOC.GetTargeSerialAsync( Strings.Target_object___ );
+
+            if ( serial == 0 )
+            {
+                UOC.SystemMessage( Strings.Invalid_or_unknown_object_id );
+                return;
+            }
+
+            Item item = Engine.Items.GetItem( serial );
+
+            if ( item == null )
+            {
+                UOC.SystemMessage( Strings.Cannot_find_item___ );
+                return;
+            }
+
+            string tiledataName = TileData.GetStaticTile( item.ID ).Name ?? "Unknown";
+
+            ScavengerEntry entry =
+                new ScavengerEntry { Enabled = true, Graphic = item.ID, Hue = item.Hue, Name = tiledataName };
+
+            Items.Add( entry );
+        }
+
+        private async Task Remove( object arg )
+        {
+            if ( !( arg is ScavengerEntry entry ) )
+            {
+                return;
+            }
+
+            Items.Remove( entry );
+
+            await Task.CompletedTask;
+        }
+
+        private async Task ClearAll( object arg )
+        {
+            Items.Clear();
+
+            await Task.CompletedTask;
+        }
+
+        public ICommand SetContainerCommand =>
+            _setContainerCommand ?? ( _setContainerCommand = new RelayCommandAsync( SetContainer, o => true ) );
+
+        private async Task SetContainer( object arg )
+        {
+            int serial = await UOC.GetTargeSerialAsync( Strings.Select_destination_container___ );
+
+            if ( serial == 0 )
+            {
+                UOC.SystemMessage( Strings.Invalid_container___ );
+                return;
+            }
+
+            ContainerSerial = serial;
+        }
+    }
+}
