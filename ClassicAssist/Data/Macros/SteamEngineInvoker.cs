d3c3cb0e4633499cc008d7e44c9be088d05d7c37
Reetus <reeeetus@gmail.com>
2020-01-20 20:50:19 +0800

    Test command implementations

--- ClassicAssist/Data/Macros/SteamEngineInvoker.cs
+++ ClassicAssist/Data/Macros/SteamEngineInvoker.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using UOSteam;
+﻿using UOSteam;
 
 namespace ClassicAssist.Data.Macros
 {
@@ -11,6 +9,12 @@ namespace ClassicAssist.Data.Macros
 
         public event PythonInvoker.dMacroException ExceptionEvent;
 
+        public SteamEngineInvoker()
+        {
+            Steam.Aliases.Register();
+            Steam.Commands.Register();
+        }
+
         public void Stop()
         {
         }
@@ -20,8 +24,6 @@ namespace ClassicAssist.Data.Macros
             ASTNode ast = Lexer.Lex( macro.Macro.Split( '\n' ) );
 
             Script script = new Script( ast );
-
-            Interpreter.RegisterCommandHandler( "sysmsg", ParseCommand );
 
             try
             {
@@ -59,125 +61,6 @@ namespace ClassicAssist.Data.Macros
             }
 
             return _instance;
-        }
-
-        private static bool ParseCommand( ref ASTNode node, bool quiet, bool force )
-        {
-            object[] param = ParseParams( node );
-
-            string text = GetParam( param, 0, string.Empty );
-            int hue = GetParam( param, 1, 946 );
-
-            UO.Commands.SystemMessage( text, hue );
-
-            node = null;
-
-            return true;
-        }
-
-        private static T GetParam<T>( IReadOnlyList<object> objects, int i, T defaultValue )
-        {
-            try
-            {
-                return (T) objects[i];
-            }
-            catch ( Exception )
-            {
-                return defaultValue;
-            }
-        }
-
-        private static object[] ParseParams( ASTNode node )
-        {
-            List<object> paramList = new List<object>();
-
-            ASTNode nextNode = node.Next();
-
-            do
-            {
-                if ( nextNode == null )
-                {
-                    break;
-                }
-
-                switch ( nextNode.Type )
-                {
-                    case ASTNodeType.IF:
-                        break;
-                    case ASTNodeType.ELSEIF:
-                        break;
-                    case ASTNodeType.ELSE:
-                        break;
-                    case ASTNodeType.ENDIF:
-                        break;
-                    case ASTNodeType.WHILE:
-                        break;
-                    case ASTNodeType.ENDWHILE:
-                        break;
-                    case ASTNodeType.FOR:
-                        break;
-                    case ASTNodeType.ENDFOR:
-                        break;
-                    case ASTNodeType.BREAK:
-                        break;
-                    case ASTNodeType.CONTINUE:
-                        break;
-                    case ASTNodeType.STOP:
-                        break;
-                    case ASTNodeType.REPLAY:
-                        break;
-                    case ASTNodeType.EQUAL:
-                        break;
-                    case ASTNodeType.NOT_EQUAL:
-                        break;
-                    case ASTNodeType.LESS_THAN:
-                        break;
-                    case ASTNodeType.LESS_THAN_OR_EQUAL:
-                        break;
-                    case ASTNodeType.GREATER_THAN:
-                        break;
-                    case ASTNodeType.GREATER_THAN_OR_EQUAL:
-                        break;
-                    case ASTNodeType.NOT:
-                        break;
-                    case ASTNodeType.AND:
-                        break;
-                    case ASTNodeType.OR:
-                        break;
-                    case ASTNodeType.STRING:
-                        paramList.Add( nextNode.Lexeme );
-                        break;
-                    case ASTNodeType.SERIAL:
-                        paramList.Add( Convert.ToInt32( nextNode.Lexeme ) );
-                        break;
-                    case ASTNodeType.INTEGER:
-                        paramList.Add( Convert.ToInt32( nextNode.Lexeme ) );
-                        break;
-                    case ASTNodeType.QUIET:
-                        break;
-                    case ASTNodeType.FORCE:
-                        break;
-                    case ASTNodeType.SCRIPT:
-                        break;
-                    case ASTNodeType.STATEMENT:
-                        break;
-                    case ASTNodeType.COMMAND:
-                        break;
-                    case ASTNodeType.LOGICAL_EXPRESSION:
-                        break;
-                    case ASTNodeType.UNARY_EXPRESSION:
-                        break;
-                    case ASTNodeType.BINARY_EXPRESSION:
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
-                nextNode = nextNode.Next();
-            }
-            while ( nextNode != null );
-
-            return paramList.ToArray();
         }
     }
 }
