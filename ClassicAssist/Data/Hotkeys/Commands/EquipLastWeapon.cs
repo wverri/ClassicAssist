f53ac9b7eaa1a90127cb5bbc52a86b08edd51f7d
Reetus <reeeetus@gmail.com>
2020-02-20 16:05:25 +0800

    HotkeyCommand Tooltip support

--- ClassicAssist/Data/Hotkeys/Commands/EquipLastWeapon.cs
+++ ClassicAssist/Data/Hotkeys/Commands/EquipLastWeapon.cs
@@ -1,8 +1,9 @@
 ﻿using ClassicAssist.Data.Macros.Commands;
+using ClassicAssist.Resources;
 
 namespace ClassicAssist.Data.Hotkeys.Commands
 {
-    [HotkeyCommand( Name = "Equip Last Weapon (Quick Weapon Switch)" )]
+    [HotkeyCommand( Name = "Equip Last Weapon (Quick Weapon Switch)", Tooltip = "Requires Server Support")]
     public class EquipLastWeapon : HotkeyCommand
     {
         public override void Execute()
