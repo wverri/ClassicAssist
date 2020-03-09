ed4715448dcd4dd9d609979eab54a757c9fc812f
Reetus <reeeetus@gmail.com>
2020-03-09 20:02:06 +0800

    Formating

--- ClassicAssist/Data/Dress/DressManager.cs
+++ ClassicAssist/Data/Dress/DressManager.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using System.Windows.Media.Animation;
 using Assistant;
 using ClassicAssist.Annotations;
 using ClassicAssist.UI.Misc;
